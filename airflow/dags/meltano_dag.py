54c396b7523f7cfbbe5387389f04a47884b04da0
guilherme.tavares@indicium.tech <112582783+guilherme-tavares-indicium@users.noreply.github.com>
2023-06-16 17:35:18 -0300

    test

--- airflow/dags/meltano_dag.py
+++ airflow/dags/meltano_dag.py
@@ -6,24 +6,24 @@ from airflow.models import Variable
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.operators.dummy_operator import DummyOperator
 
-def create_new_kubernetes_operator_task(task_id, output, dag, task_no):
-    return KubernetesPodOperator(
-        task_id=task_id,
-        name=f'run-stream-{task_no}',
-        namespace='prod-airflow',
-        image='046390580407.dkr.ecr.us-east-1.amazonaws.com/hyland_aws_meltano_ecr',
-        image_pull_policy='Always',
-        is_delete_operator_pod=True,
-        dag=dag,
-        cmds=['/bin/bash', '-c'],
-        arguments=[f'meltano select tap-github_issues $STREAMNAME "*" && meltano run tap-github_issues target-s3'],
-        env_vars={
-            "AWS_ID": Variable.get("AWS_ID"),
-            "AWS_PSW": Variable.get("AWS_PSW"),
-            "GITHUB_TOKEN" : Variable.get("GITHUB_TOKEN"),
-            "STREAMNAME": output
-        },
-    )
+# def create_new_kubernetes_operator_task(task_id, output, dag, task_no):
+#     return KubernetesPodOperator(
+#         task_id=task_id,
+#         name=f'run-stream-{task_no}',
+#         namespace='prod-airflow',
+#         image='046390580407.dkr.ecr.us-east-1.amazonaws.com/hyland_aws_meltano_ecr',
+#         image_pull_policy='Always',
+#         is_delete_operator_pod=True,
+#         dag=dag,
+#         cmds=['/bin/bash', '-c'],
+#         arguments=[f'meltano select tap-github_issues $STREAMNAME "*" && meltano run tap-github_issues target-s3'],
+#         env_vars={
+#             "AWS_ID": Variable.get("AWS_ID"),
+#             "AWS_PSW": Variable.get("AWS_PSW"),
+#             "GITHUB_TOKEN" : Variable.get("GITHUB_TOKEN"),
+#             "STREAMNAME": output
+#         },
+#     )
 
 # DAG
 default_args = {
@@ -47,11 +47,13 @@ with DAG(
     catchup=False
 ) as dag:
     
+    start = DummyOperator(task_id='run_this_first', dag=dag)
+
     run_meltano_extract = KubernetesPodOperator(
         task_id='run_meltano_extraction',
         name='run-container-extraction',
         namespace='prod-airflow',
-        image='196029031078.dkr.ecr.us-east-1.amazonaws.com/prod-meltano-hylandtraining',
+        image='196029031078.dkr.ecr.us-east-1.amazonaws.com/prod-meltano-hylandtraining:5ba8dc20a968fe5fd0512d43d41866f83779d917',
         image_pull_policy='Always',
         is_delete_operator_pod=True,
         dag=dag,
@@ -66,4 +68,4 @@ with DAG(
         },
     )
 
-run_meltano_extract
+start >> run_meltano_extract
