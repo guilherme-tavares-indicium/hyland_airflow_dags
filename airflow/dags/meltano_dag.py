26261101f67b2a6adb69c7f12bc6118e78e70e64
guilherme.tavares@indicium.tech <112582783+guilherme-tavares-indicium@users.noreply.github.com>
2023-06-16 21:34:39 -0300

    test

--- airflow/dags/meltano_dag.py
+++ airflow/dags/meltano_dag.py
@@ -11,7 +11,12 @@ def create_task_for_stream(dag, stream_name, stream_no):
         task_id=f'run_meltano_extraction_{stream_no}',
         name=f'run-container-extraction-{stream_no}',
         namespace='prod-airflow',
-        # ...
+        image='196029031078.dkr.ecr.us-east-1.amazonaws.com/prod-meltano-hylandtraining:5ba8dc20a968fe5fd0512d43d41866f83779d917',
+        image_pull_policy='Always',
+        is_delete_operator_pod=True,
+        dag=dag,
+        cmds=['/bin/bash', '-c'],
+        arguments=['echo ${STREAMNAME}'],
         env_vars={
             "AWS_ID": Variable.get("AWS_ID"),
             "AWS_PSW": Variable.get("AWS_PSW"),
@@ -37,11 +42,24 @@ def task_builder(ti):
 
 with dag:
     start = DummyOperator(task_id='run_this_first')
+
     get_stream_list = KubernetesPodOperator(
         task_id='run_meltano_extraction',
         name='run-container-extraction',
         namespace='prod-airflow',
-        # ...
+        image='196029031078.dkr.ecr.us-east-1.amazonaws.com/prod-meltano-hylandtraining:5ba8dc20a968fe5fd0512d43d41866f83779d917',
+        image_pull_policy='Always',
+        is_delete_operator_pod=True,
+        dag=dag,
+        cmds=['/bin/bash', '-c'],
+        # arguments=['meltano select tap-github_issues meltano_contributors "*" && meltano run tap-github_issues target-jsonl'],
+        arguments=['python get_streams.py tap-github_issues'],
+        env_vars={
+            "AWS_ID": Variable.get("AWS_ID"),
+            "AWS_PSW": Variable.get("AWS_PSW"),
+            "GITHUB_TOKEN" : Variable.get("GITHUB_TOKEN"),
+            "STREAMNAME": "meltano_contributors"
+        },
         do_xcom_push=True
     )
 
@@ -53,7 +71,3 @@ with dag:
     )
 
     start >> get_stream_list >> create_tasks
-
-
-
-
