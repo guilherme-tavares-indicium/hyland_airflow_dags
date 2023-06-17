55b670a4c39a3d3682b4ee3f333c429bd7717d84
guilherme.tavares@indicium.tech <112582783+guilherme-tavares-indicium@users.noreply.github.com>
2023-06-16 23:26:24 -0300

    test

--- airflow/dags/new_meltano_dag.py
+++ airflow/dags/new_meltano_dag.py
@@ -3,17 +3,34 @@ from airflow import DAG
 from airflow.models import Variable
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python import PythonOperator
+import json
 
 def create_task_for_stream(dag, stream_name, stream_no):
     task = KubernetesPodOperator(
-        # ... task definition remains the same
+        task_id=f'run_meltano_extraction_{stream_no}',
+        name=f'run-container-extraction-{stream_no}',
+        namespace='prod-airflow',
+        image='196029031078.dkr.ecr.us-east-1.amazonaws.com/prod-meltano-hylandtraining:5ba8dc20a968fe5fd0512d43d41866f83779d917',
+        image_pull_policy='Always',
+        is_delete_operator_pod=True,
+        dag=dag,
+        cmds=['/bin/bash', '-c'],
+        arguments=['echo ${STREAMNAME}'],
+        env_vars={
+            "AWS_ID": Variable.get("AWS_ID"),
+            "AWS_PSW": Variable.get("AWS_PSW"),
+            "GITHUB_TOKEN": Variable.get("GITHUB_TOKEN"),
+            "STREAMNAME": stream_name
+        },
+        do_xcom_push=False,
     )
     return task
 
+
 def create_downstream_tasks(ti):
-    xcom_output = ti.xcom_pull(task_ids='get_stream_list')
-    streams = xcom_output.get('return_value')
+    xcom_output = ti.xcom_pull(task_ids='run_meltano_extraction')
+    streams = json.loads(xcom_output)
     
     downstream_tasks = []
     for i, stream_name in enumerate(streams):
@@ -24,10 +41,10 @@ def create_downstream_tasks(ti):
     ti.xcom_push(key='downstream_tasks', value=downstream_tasks)
 
 def set_downstream_tasks(ti):
-    downstream_tasks = ti.xcom_pull(key='downstream_tasks', task_ids='create_tasks2')
+    downstream_tasks = ti.xcom_pull(key='downstream_tasks', task_ids='create_tasks')
     
     for task in downstream_tasks:
-        task.set_downstream(ti.task)
+        task.set_upstream(ti.task)
 
 default_args = {
     "owner": "airflow",
@@ -39,7 +56,7 @@ default_args = {
 }
 
 with DAG(
-    'Meltano_Tap-S3_github_NEW',
+    'Meltano_Tap-S3_github',
     default_args=default_args,
     description='Dag to run meltano using docker',
     schedule_interval='@once',
@@ -51,18 +68,33 @@ with DAG(
     start = DummyOperator(task_id='run_this_first')
 
     get_stream_list = KubernetesPodOperator(
-        # ... KubernetesPodOperator definition remains the same
+        task_id='run_meltano_extraction',
+        name='run-container-extraction',
+        namespace='prod-airflow',
+        image='196029031078.dkr.ecr.us-east-1.amazonaws.com/prod-meltano-hylandtraining:5ba8dc20a968fe5fd0512d43d41866f83779d917',
+        image_pull_policy='Always',
+        is_delete_operator_pod=True,
+        dag=dag,
+        cmds=['/bin/bash', '-c'],
+        arguments=['python get_streams.py tap-github_issues'],
+        env_vars={
+            "AWS_ID": Variable.get("AWS_ID"),
+            "AWS_PSW": Variable.get("AWS_PSW"),
+            "GITHUB_TOKEN": Variable.get("GITHUB_TOKEN"),
+            "STREAMNAME": "meltano_contributors"
+        },
+        do_xcom_push=True
     )
 
     create_tasks = PythonOperator(
-        task_id='create_tasks2',
+        task_id='create_tasks',
         python_callable=create_downstream_tasks,
         provide_context=True,
         dag=dag,
     )
 
     set_downstream = PythonOperator(
-        task_id='set_downstream_tasks2',
+        task_id='set_downstream_tasks',
         python_callable=set_downstream_tasks,
         provide_context=True,
         dag=dag,
